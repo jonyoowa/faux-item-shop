f7d901f515a30557f188d39f3181cd182a47a660
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2020-10-07 13:10:51 -0700

    Enabled ability to create an account

--- components/registerForm.js
+++ components/registerForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function LoginForm({ email, setEmail, password, setPassword, handleLogin }) {
+export default function RegisterForm({ username, setUsername, email, setEmail, password, setPassword, createNewAccount }) {
     return (
         <div className="container mx-auto px-64">
-            <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
+            <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={createNewAccount}>
+
+                <div className="mb-4">
+                    <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" onChange={(e) => setUsername(e.target.value)} value={username} />
+                </div>
+
                 <div className="mb-4">
                     <label className="block text-white text-sm font-bold mb-2" htmlFor="email">
                         Email
@@ -18,9 +26,9 @@ export default function LoginForm({ email, setEmail, password, setPassword, hand
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Sign in</button>
-                    <Link href="/register">
-                        <a className="inline-block align-baseline font-bold text-sm text-white hover:text-gray-400" href="#">Create account</a>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Create account</button>
+                    <Link href="/">
+                        <a className="inline-block align-baseline font-bold text-sm text-red-600 hover:text-red-400">Cancel</a>
                     </Link>
                 </div>
             </form>
---
+++ pages/register.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+import Head from 'next/head'
+import Layout from '../components/layout';
+import RegisterForm from '../components/registerForm';
+
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+export default function Register() {
+    const router = useRouter();
+    const registerEndpoint = `${process.env.NEXT_PUBLIC_HOSTNAME}:${process.env.NEXT_PUBLIC_PORT}/auth/local/register`;
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const createNewAccount = async (e) => {
+        e.preventDefault();
+        axios
+            .post(registerEndpoint, {
+                username,
+                email,
+                password,
+            })
+            .then(res => {
+                window.sessionStorage.setItem("jwt", res.data.jwt);
+                router.push("/success");
+            })
+            .catch(err => {
+                console.log(`An error has occured: ${err}`);
+            })
+    }
+
+    useEffect(() => {
+        const jwt = window.sessionStorage.getItem("jwt");
+        if (jwt) {
+            router.push("/success");
+        }
+    }, []);
+
+    return (
+        <Layout>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <RegisterForm username={username} setUsername={setUsername} email={email} setEmail={setEmail} password={password} setPassword={setPassword} createNewAccount={createNewAccount} />
+        </Layout>
+    )
+}
