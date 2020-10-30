7f038f03ecd91aa2c09b397d63fafaa501dd6416
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2020-10-30 16:03:09 -0700

    Created functioning Numbers Form

--- components/storedNumbersForm.js
+++ components/storedNumbersForm.js
@@ -1,13 +1,30 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Header from './header';
-import styles from './layout.module.css';
+import React from 'react';
 
-export default function StoredNumbersForm() {
+export default function StoredNumbersForm({ storeNumber, setShowTable, number, setNumber }) {
 
     return (
-        <div className="container mx-auto px-64">
-            <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={() => console.log("test")}>
+        <div className="w-full max-w-xs mx-auto">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={storeNumber}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="storedNumber">
+                        Number
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="storedNumber"
+                        type="number"
+                        onChange={(e) => setNumber(e.target.value)}
+                        value={number}
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Add
+                    </button>
+                    <button className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" onClick={() => console.log("Change this to show table")}>
+                        Show Table
+                    </button>
+                </div>
             </form>
         </div>
     )
--- pages/numbers.js
+++ pages/numbers.js
@@ -6,6 +6,27 @@ import { useRouter } from 'next/router';
 
 export default function Numbers() {
     const router = useRouter();
+    const storedNumbersEndpoint = `${process.env.NEXT_PUBLIC_HOSTNAME}:${process.env.NEXT_PUBLIC_PORT}/stored-numbers`;
+
+    const [showTable, setShowTable] = useState(false);
+    const [number, setNumber] = useState(0);
+
+    const storeNumber = async (e) => {
+        e.preventDefault();
+        axios
+            .post(storedNumbersEndpoint, {
+                storedValue: number,
+            })
+            .then(res => {
+                console.log(res.data);
+                console.log("Successfully stored number");
+            })
+            .catch(err => {
+                let errH = JSON.stringify(err.response);
+                console.log(`An error has occured: ${errH}`);
+            })
+        setNumber(0);
+    }
 
     useEffect(() => {
         // Redirect user to homepage if not logged in
@@ -27,7 +48,7 @@ export default function Numbers() {
     return (
         <Layout>
             <div className="flex flex-wrap">
-                <StoredNumbersForm />
+                {showTable ? <StoredNumbersTable /> : <StoredNumbersForm storeNumber={storeNumber} setShowTable={setShowTable} number={number} setNumber={setNumber} />}
             </div>
         </Layout>
     )
