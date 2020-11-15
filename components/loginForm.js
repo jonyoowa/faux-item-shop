f5294fa2a5e97afa081a7f1f8e8f44fe378d5ea9
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2020-11-15 14:03:28 -0800

    Modified login form style

--- components/loginForm.js
+++ components/loginForm.js
@@ -3,24 +3,43 @@ import Link from 'next/link';
 
 export default function LoginForm({ email, setEmail, password, setPassword, handleLogin }) {
     return (
-        <div className="container mx-auto px-64">
+        <div className="box-border h-auto w-1/5 min-w-32">
             <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
+                <div className="mb-6">
+                    <span className="text-white text-2xl font-medium">Member Login</span>
+                </div>
                 <div className="mb-4">
                     <label className="block text-white text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
                 </div>
                 <div className="mb-6">
                     <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Sign in</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Sign in
+                    </button>
                     <Link href="/register">
-                        <a className="inline-block align-baseline font-bold text-sm text-white hover:text-gray-400" href="#">Create account</a>
+                        <a className="inline-block align-baseline font-bold text-sm text-white hover:text-gray-400" href="#">
+                            Create account
+                        </a>
                     </Link>
                 </div>
             </form>
