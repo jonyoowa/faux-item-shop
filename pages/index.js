c72d19933b6e2e1e82e51803557edf603ffd4f38
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2020-11-15 16:57:16 -0800

    Finished rudimentary sign in page

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import styles from '../components/layout.module.css';
 
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function Home() {
   const router = useRouter();
@@ -45,15 +46,17 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-full min-h-screen flex flex-wrap justify-center items-center p-16">
-
-        {/* <h1>
-          Welcome to the Faux Item Shop
-        </h1> */}
-
-        <div className="rounded-lg bg-white h-auto w-3/5 flex flex-wrap justify-around px-8 py-24">
-          <div className="flex-none h-auto w-1/4 bg-red-500"></div>
-          <LoginForm email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleLogin={handleLogin} />
+      <div className="w-full min-h-screen p-16">
+        <div className="justify-center flex mb-12">
+          <span className="text-5xl font-semibold">Welcome to the Faux Item Shop</span>
+        </div>
+        <div className="justify-center items-center flex flex-wrap">
+          <div className="rounded-lg bg-white h-auto w-3/5 flex flex-wrap justify-around px-8 py-24">
+            <div className="flex-none h-auto w-1/4">
+              <Image src="/undraw_Online_shopping_re_k1sv.svg" width={249} height={338} alt="Online shopping" />
+            </div>
+            <LoginForm email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleLogin={handleLogin} />
+          </div>
         </div>
       </div>
     </div>
